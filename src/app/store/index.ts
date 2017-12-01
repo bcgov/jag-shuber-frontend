9b4825b9e6f3a93417c232c4fcd31068e3734e2a
Rough Draft <roughdraft@cjams-mac.short.st>
2017-12-01 08:55:40 -0800

    Different logo and dev tools

--- src/app/store/index.ts
+++ src/app/store/index.ts
@@ -1,4 +1,4 @@
-import {createStore,applyMiddleware} from 'redux'
+import {createStore,applyMiddleware,compose} from 'redux'
 import {default as thunk,ThunkAction as _ThunkAction} from 'redux-thunk'
 import {default as reducers,RootState} from './reducers'
 import {default as api,API} from '../api'
@@ -38,9 +38,15 @@ const initialState:any = {
 
 export type ThunkAction = ()=>_ThunkAction<any,RootState,{api:API}>;
 
-const store = createStore(reducers,
-    initialState,
-    applyMiddleware(thunk.withExtraArgument({api}))
-);
+let thisWindow:any = window;
+
+const composeEnhancers = thisWindow.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const enhancers = composeEnhancers(
+    applyMiddleware(
+        thunk.withExtraArgument({api})
+    )
+)
+
+const store = createStore(reducers,initialState,enhancers);
 
 export default store;
