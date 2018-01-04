7e1f7366442a96243541b1757d591f1f87e2e45b
Holly Grayson <Holly.Grayson@gds.CA>
2018-01-04 14:13:41 -0800

    updating unit tests for validators

--- src/app/infrastructure/Validators.ts
+++ src/app/infrastructure/Validators.ts
@@ -1,35 +1,18 @@
-//const required = (value:any) => value ? undefined : 'Required'
-//const required = (value:any) => {}
 export const VALIDATOR_MESSAGES = {
-    INVALID_INTEGER : "Must be an integer."
+    INVALID_INTEGER: "Must be an integer.",
+    INVALID_NUMBER: "Must be a number.",
+    REQUIRED_VALUE: "This is a required field."
     
 }
 
-export function required(value:any){
-    if(value){
-        return undefined;
-    }
-    return 'Required';
+export const required = (value?:any) => value || value===false ? undefined :  VALIDATOR_MESSAGES.REQUIRED_VALUE;
 
-    //return value ? undefined : 'Required';
-}
-
-function maxLength (max:number){
-    const validator = function (value:any){
-        value && value.length > max ? `Must be ${max} characters or less` : undefined
-    }
-    return validator;
-} 
-
-
- const maxNameLength = 15;
- export const maxLength15 = maxLength(maxNameLength);
- export const maxLength10 = maxLength(10);
 
 export const integer = (value:any) =>{ 
     let invalidMessage = number(value);
     if(!invalidMessage){
-        if(Math.floor(value)===value){
+        const numericValue = Number(value);
+        if(Math.floor(numericValue)===numericValue){
             invalidMessage = undefined;
         }else{
              invalidMessage = VALIDATOR_MESSAGES.INVALID_INTEGER;
@@ -39,16 +22,19 @@ export const integer = (value:any) =>{
 }
 
 export const number = (value:any) =>{ 
-    return value && isNaN(Number(value)) ? 'Must be a number' : undefined;
+    return value && isNaN(value) ? VALIDATOR_MESSAGES.INVALID_NUMBER : undefined;
 }
-// const minValue = min => value =>
-//   value && value < min ? `Must be at least ${min}` : undefined
-// const minValue18 = minValue(18)
-// const email = value =>
-//   value && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value) ?
-//   'Invalid email address' : undefined
-// const tooOld = value =>
-//   value && value > 65 ? 'You might be too old for this' : undefined
-// const aol = value =>
-//   value && /.+@aol\.com/.test(value) ?
-//   'Really? You still use AOL for your email?' : undefined
+
+/* examples of other validators that can be used as needed
+
+const minValue = min => value =>
+  value && value < min ? `Must be at least ${min}` : undefined
+const minValue18 = minValue(18)
+const email = value =>
+  value && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value) ?
+  'Invalid email address' : undefined
+const tooOld = value =>
+  value && value > 65 ? 'You might be too old for this' : undefined
+const aol = value =>
+  value && /.+@aol\.com/.test(value) ?
+  'Really? You still use AOL for your email?' : undefined */
--- src/app/infrastructure/__tests__/ValidatorTest.ts
+++ src/app/infrastructure/__tests__/ValidatorTest.ts
@@ -5,18 +5,104 @@ describe('Integer Validator', () => {
     const validator = Validators.integer;
     it('Should return undefined given an integer', () => {
         expect(validator(10)).toBeUndefined();
+        expect(validator('14')).toBeUndefined();
     })
 
     it('Should return an error message given a decimal value', () => {
         expect(validator(10.987)).toBe(Validators.VALIDATOR_MESSAGES.INVALID_INTEGER);
+        expect(validator('10.987')).toBe(Validators.VALIDATOR_MESSAGES.INVALID_INTEGER);
     })
 
-    it('Should return an error message given anything but an integer', () => {
-        // expect(validator('10')).toBeDefined(); this is "a number" need to fix this
+    it('Should return an error message given a string', () => {
         expect(validator('random text')).toBeDefined();
+    })
+
+    it('Should return an error message given an array', () => {
         expect(validator(['array', 'of', 'strings'])).toBeDefined();
+    })
+
+    it('Should return an error message given an object', () => {
         expect(validator({ key: 'value' })).toBeDefined();
     })
 
 })
 
+describe('Number Validator', () => {
+    const validator = Validators.number;
+    it('Should return undefined given an integer', () => {
+        expect(validator(10)).toBeUndefined();
+        expect(validator(-874)).toBeUndefined();
+    })
+
+    it('Should return undefined given a decimal value', () => {
+        expect(validator(10.5)).toBeUndefined();
+        expect(validator(-10.5)).toBeUndefined();
+    })
+
+    it('Should return undefined given a string that contains only an integer', () => {
+        expect(validator('14')).toBeUndefined();
+        expect(validator('14.5')).toBeUndefined();       
+    })
+
+    it('Should return undefined given a string that contains only a decimal value', () => {
+        expect(validator('14.45')).toBeUndefined();
+        expect(validator('-14.45')).toBeUndefined();       
+    })
+
+    it('Should return an error message given a string', () => {
+        expect(validator('random text')).toBeDefined();
+    })
+
+    it('Should return an error message given an array', () => {
+        expect(validator(['array', 'of', 'strings'])).toBeDefined();
+    })
+
+    it('Should return an error message given an object', () => {
+        expect(validator({ key: 'value' })).toBeDefined();
+    })
+
+})
+
+describe('Required Validator', () => {
+    const validator = Validators.required;
+    it('Should return undefined given a number', () => {
+        expect(validator(10)).toBeUndefined();
+    })
+
+    it('Should return undefined given a string', () => {
+        expect(validator('random text')).toBeUndefined();
+    })
+
+    it('Should return undefined given an array', () => {
+        expect(validator(['array', 'of', 'strings'])).toBeUndefined();
+    })
+
+    it('Should return undefined given an object', () => {
+        expect(validator({ key: 'value' })).toBeUndefined();
+    })
+
+    it('Should return undefined given true', () => {
+       expect(validator(true)).toBeUndefined();        
+    })
+
+    it('Should return undefined given false', () => {
+        expect(validator(false)).toBeUndefined();        
+    })
+
+    it('Should return an error message given a null value', () => {
+        expect(validator(null)).toBeDefined();
+    })
+
+    it('Should return an error message given an empty string', () => {
+        expect(validator('')).toBeDefined();
+    })
+
+    it('Should return an error message given nothing', () => {
+        expect(validator()).toBeDefined();
+    })
+
+    it('Should return an error message given undefined', () => {
+        expect(validator(undefined)).toBeDefined();
+    })
+
+})
