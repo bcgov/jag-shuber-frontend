1db8f2c7333f063a24e544627a344759460709ee
Holly Grayson <Holly.Grayson@gds.CA>
2018-01-04 14:48:05 -0800

    removing comments

--- src/app/modules/sheriffs/containers/CreateSheriffForm.tsx
+++ src/app/modules/sheriffs/containers/CreateSheriffForm.tsx
@@ -2,23 +2,6 @@ import { reduxForm } from 'redux-form';
 import { default as SheriffForm, SheriffFormProps } from '../components/SheriffForm';
 import { createSheriff } from '../actions';
 import { SheriffAbility, Sheriff } from '../../../api/index';
-// import { connect } from 'react-redux';
-
-// class CreateSheriffForm extends SheriffForm{
-//     constructor(props){
-//         super(props);
-//         this.state = {
-
-//         }
-//     }
-
-// }
-
-// const mapStateToProps = (state:RootState,props:SheriffFormProps)=>{
-//     // return your selector stuff
-// }
-
-// const connectedSheriffForm = connect<SheriffFormProps>(mapStateToProps)(CreateSheriffForm)
 
 const createSheriffFormFactory = reduxForm<SheriffFormProps, any>({ 
     form: 'CreateSheriff',  
