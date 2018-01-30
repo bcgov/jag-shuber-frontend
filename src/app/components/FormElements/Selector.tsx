673233bc999616738bf87d1ca89b621af48179c7
Holly Grayson <Holly.Grayson@gds.CA>
2018-01-30 09:44:01 -0800

    Creating generic selector

--- src/app/components/FormElements/Selector.tsx
+++ src/app/components/FormElements/Selector.tsx
@@ -2,39 +2,18 @@ import * as React from 'react';
 import { default as FormFieldWrapper, FormFieldWrapperProps } from './FormFieldWrapper';
 import { FormControl } from 'react-bootstrap';
 
-
 export interface SelectorProps {
-
+    data: {key:string|number, value:string}[]; 
 }
 
-export default class Selector extends React.Component<FormFieldWrapperProps, SelectorProps>{
+export default class Selector extends React.PureComponent<FormFieldWrapperProps & SelectorProps>{
     render(){
-        const { input:{value, onChange}, label} = this.props;
+        const { input:{value, onChange}, label, data} = this.props;
         return (
             <FormFieldWrapper {...this.props}>
                 <FormControl componentClass="select" value={value} onChange={onChange}>
-                    <option value="No courthouse selected">{`Select ${label}`}</option>
-                    <option value="Campbell River">Campbell River</option>
-                    <option value="Chilliwack">Chilliwack</option>
-                    <option value="Courtenay">Courtenay</option>
-                    <option value="Duncan">Duncan</option>
-                    <option value="Gold River">Gold River</option>
-                    <option value="Kamloops">Kamloops</option>
-                    <option value="Nanaimo">Nanaimo</option>
-                    <option value="New Westminster">New Westminster</option>
-                    <option value="North Vancouver">North Vancouver</option>
-                    <option value="Port Alberni">Port Alberni</option>
-                    <option value="Port Coquitlam">Port Coquitlam</option>
-                    <option value="Port Hardy">Port Hardy</option>
-                    <option value="Powell River">Powell River</option>
-                    <option value="Prince George">Prince George</option>
-                    <option value="Richmond">Richmond</option>
-                    <option value="Surrey">Surrey</option>
-                    <option value="Sidney">Sidney</option>
-                    <option value="Vancouver - 222 Main">Vancouver - 222 Main</option>
-                    <option value="Vancouver - VLC">Vancouver - VLC</option>
-                    <option value="Vancouver Robson Square">Vancouver Robson Square</option>
-                    <option value="Victoria">Victoria</option>
+                    <option>{`Select ${label}`}</option>
+                    {data.map((keyValue, index)=><option value={keyValue.key}>{keyValue.value}</option>)}
                 </FormControl>
             </FormFieldWrapper>
         );
--- src/app/components/FormElements/TrainingTypeSelector.tsx
+++ src/app/components/FormElements/TrainingTypeSelector.tsx
@@ -1,19 +1,14 @@
 import * as React from 'react';
-import { default as FormFieldWrapper, FormFieldWrapperProps } from './FormFieldWrapper';
-import { FormControl } from 'react-bootstrap';
+import { FormFieldWrapperProps } from './FormFieldWrapper';
 import { TRAINING_TYPES } from '../../api';
+import Selector from './Selector';
 
 
 export default class TrainingTypeSelector extends React.PureComponent<FormFieldWrapperProps>{
-    render() {
-        const { input: { value, onChange }, label } = this.props;
+    render(){        
+        const data = Object.keys(TRAINING_TYPES).map((key, index)=>({key, value:TRAINING_TYPES[key]}));
         return (
-            <FormFieldWrapper {...this.props}>
-                <FormControl componentClass="select" value={value} onChange={onChange}>
-                    <option value="No training type selected">{`Select ${label}`}</option>
-                    {Object.keys(TRAINING_TYPES).map((k, i)=><option value={k}>{TRAINING_TYPES[k]}</option>)}
-                </FormControl>
-            </FormFieldWrapper>
+            <Selector data={data} {...this.props}/>
         );
     }
 }
