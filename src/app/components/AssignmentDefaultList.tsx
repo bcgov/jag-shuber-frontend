c500036a84d5c819b3dd5ff656556a7e3ad57e71
Holly Grayson <Holly.Grayson@gds.CA>
2018-02-02 15:12:30 -0800

    Adjusting contents of the AssignmentDefaultList component

--- src/app/components/AssignmentDefaultList.tsx
+++ src/app/components/AssignmentDefaultList.tsx
@@ -1,9 +1,7 @@
 import * as React from 'react';
 import {
-    Well,
     Table
 } from 'react-bootstrap';
-import AddAssignmentModal from '../containers/AddAssignmentModal';
 import AssignmentDefaultRecurrenceDetails from './AssignmentDefaultRecurrenceDetails';
 import {
     DEFAULT_ASSIGNMENTS,
@@ -13,40 +11,30 @@ import {
 export default class AssignmentDefaultList extends React.PureComponent<any> {
     render() {
         return (
-            <div style={{ display: "flex", justifyContent: "center" }}>
-                <Well style={{ display: "flex", backgroundColor: 'white', flexDirection: "column", flex: "1 1", maxWidth: "80%", minWidth: 800 }}>
-                    <div style={{ flex: "1", alignSelf: "center", paddingBottom: 15 }}>
-                        <h1>Manage Default Assignments</h1>
-                    </div>
-                    <Table responsive>
-                        <thead>
+            <Table responsive>
+                <thead>
+                    <tr>
+                        <th className="text-left">Assignment</th>
+                        <th className="text-left">Work Section</th>
+                        <th className="text-left">Default Days &amp; Times</th>
+                        <th className="text-left"># of Sheriffs</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {DEFAULT_ASSIGNMENTS.map(function (assignment) {
+                        return (
                             <tr>
-                                <th className="text-left">Assignment</th>
-                                <th className="text-left">Work Section</th>
-                                <th className="text-left">Default Days &amp; Times</th>
-                                <th className="text-left"># of Sheriffs</th>
+                                <td><strong>{assignment.assignmentTemplate.title}</strong></td>
+                                <td>{assignment.assignmentTemplate.workSectionId && WORK_SECTIONS[assignment.assignmentTemplate.workSectionId]}</td>
+                                <td>
+                                    {assignment.recurrenceInfo.map((recurrence) => <AssignmentDefaultRecurrenceDetails data={recurrence} />)}
+                                </td>
+                                <td>{assignment.assignmentTemplate.sherrifsRequired}</td>
                             </tr>
-                        </thead>
-                        <tbody>
-                            {DEFAULT_ASSIGNMENTS.map(function (assignment) {
-                                return (
-                                    <tr>
-                                        <td><strong>{assignment.assignmentTemplate.title}</strong></td>
-                                        <td>{assignment.assignmentTemplate.workSectionId && WORK_SECTIONS[assignment.assignmentTemplate.workSectionId]}</td>
-                                        <td>
-                                            {assignment.recurrenceInfo.map((recurrence) => <AssignmentDefaultRecurrenceDetails data = {recurrence}/> )}
-                                        </td>
-                                        <td>{assignment.assignmentTemplate.sherrifsRequired}</td>
-                                    </tr>
-                                );
-                            })}
-                        </tbody>
-                    </Table>
-                    <div style={{ textAlign: "right" }}>
-                        <AddAssignmentModal />
-                    </div>
-                </Well>
-            </div>
+                        );
+                    })}
+                </tbody>
+            </Table>
         );
     }
 }
--- src/app/pages/DefaultAssignments.tsx
+++ src/app/pages/DefaultAssignments.tsx
@@ -1,10 +1,25 @@
 import * as React from 'react';
+import {
+    Well
+} from 'react-bootstrap';
+import AddAssignmentModal from '../containers/AddAssignmentModal';
 import AssignmentDefaultList from '../components/AssignmentDefaultList';
 
 class DefaultAssignments extends React.PureComponent {
     render() {
         return (
-            <AssignmentDefaultList />
+
+            <div style={{ display: "flex", justifyContent: "center" }}>
+                <Well style={{ display: "flex", backgroundColor: 'white', flexDirection: "column", flex: "1 1", maxWidth: "80%", minWidth: 800 }}>
+                    <div style={{ flex: "1", alignSelf: "center", paddingBottom: 15 }}>
+                        <h1>Manage Default Assignments</h1>
+                    </div>
+                    <AssignmentDefaultList />
+                    <div style={{ textAlign: "right" }}>
+                        <AddAssignmentModal />
+                    </div>
+                </Well>
+            </div>
         );
     }
 }
