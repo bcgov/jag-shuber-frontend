7ac41a430f89e51ea9978b51b712e395a5fac2a5
Holly Grayson <Holly.Grayson@gds.CA>
2018-02-05 10:29:57 -0800

    Removed 'Weekdays' and 'Everyday' from weekday checklist

--- src/app/components/FormElements/DaysOfWeekChecklist.tsx
+++ src/app/components/FormElements/DaysOfWeekChecklist.tsx
@@ -11,7 +11,7 @@ export default class DaysOfWeekChecklist extends React.PureComponent<FormFieldWr
         const { input: { onChange } } = this.props;
         return (
             <FormFieldWrapper {...this.props}>
-                <br/>{EnumUtils.getEnumKeysAsStrings(DaysOfWeek).map((k) =>
+                <br/>{EnumUtils.getEnumKeysAsStrings(DaysOfWeek).filter( k => k != 'Weekdays' && k != 'Everyday').map((k) =>
                     <Checkbox inline onChange={onChange} value={k}>{k}</Checkbox>)} {' '}
             </FormFieldWrapper>
 
