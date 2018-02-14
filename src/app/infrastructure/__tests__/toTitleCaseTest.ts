81a6eebcaefd556e8637217f1b722600d151f2df
Colter McQuay <colter.mcquay@roughdraftdev.com>
2018-02-14 09:11:21 -0800

    adding test for toTitleCase

---
+++ src/app/infrastructure/__tests__/toTitleCaseTest.ts
@@ -0,0 +1,17 @@
+import toTitleCase from '../toTitleCase';
+
+describe('toTitleCase() ', () => {
+    const expected = "Hello World This Is Title Case";
+    
+    it('Should convert lowercase to titleCase', () => {
+        expect(toTitleCase(expected.toLowerCase())).toEqual(expected);        
+    });
+
+    it('Should convert uppercase to titleCase', () => {
+        expect(toTitleCase(expected.toUpperCase())).toEqual(expected);        
+    });
+
+    it('Should convert mixed to titleCase', () => {
+        expect(toTitleCase("heLLo World tHis iS TITLE cAsE")).toEqual(expected);        
+    });
+})
