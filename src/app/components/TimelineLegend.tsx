c2d2b595b595bf17c2c33424ee36a00eca57b088
Colter McQuay <colter.mcquay@roughdraftdev.com>
2018-02-20 09:23:09 -0800

    Comitting to pass off to holly

---
+++ src/app/components/TimelineLegend.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+
+export default class TimelineLegend extends React.PureComponent {
+    render() {
+        return (
+            <div style={{
+                background: "#003366",
+                flex: "0 0 auto",
+                border: "0px",
+                color: "#FFF",
+                textAlign: 'left',
+                padding: "20px"
+            }}>
+                {this.props.children}
+            </div>
+        );
+    }
+}
--- src/app/pages/Timeline.tsx
+++ src/app/pages/Timeline.tsx
@@ -1,7 +1,11 @@
 import * as React from "react"
 import DailyTimeline from "../containers/DailyTimeline/DailyTimeline";
-import { default as AssignmentAddModal } from '../containers/AssignmentAddModal';
-import { Glyphicon } from "react-bootstrap";
+import TimelineToolsPanel from "../components/TimelineToolsPanel";
+import SheriffList from "../containers/SheriffList";
+import { Sheriff } from "../api";
+import SheriffListCard from "../components/SheriffListCard";
+
+
 class TimelinePage extends React.PureComponent {
     render() {
         return (
@@ -9,37 +13,9 @@ class TimelinePage extends React.PureComponent {
                 <div style={{ flex: "1 0px", minWidth: 500 }}>
                     <DailyTimeline />
                 </div>
-                <div style={{
-                    flex:"0 200px",                    
-                    display: 'flex',
-                    flexDirection: 'column',
-                    alignItems: 'stretch',
-                    borderLeft: "2px solid #BBB",
-                    borderBottom: "2px solid #BBB"
-                }}>
-                    <div style={{
-                        background: "#003366",
-                        flex: "0 0 60px",
-                        border: "0px",
-                        color: "#FFF",
-                        textAlign: 'center'                        
-                    }}>
-                        <h3 style={{lineHeight:"60px",padding:0,margin:0,color: "#FFF" }}>Tools</h3>
-                    </div>
-                    <div style={{ flex: "1 1 auto", padding: 10, textAlign: "center" }}>
-                        <AssignmentAddModal />
-                    </div>
-                    <div style={{
-                        background: "#003366",
-                        flex: "0 0 auto",
-                        border: "0px",
-                        color: "#FFF",
-                        textAlign: 'left',
-                        padding: "20px"                        
-                    }}>
-                        <Glyphicon glyph="asterisk"/> = Assignment Court
-                    </div>
-                </div>
+                <TimelineToolsPanel titleText="Sheriffs" legendChildren={<div>Hello Legend</div>}>
+                    <SheriffList SheriffRenderer={(s:Sheriff)=><SheriffListCard sheriff={s}/>}/>
+                </TimelineToolsPanel>
             </div>
         );
     }
