f8092176e74f318cb4c01848a8ec4259f8fa35fe
Holly Grayson <Holly.Grayson@gds.CA>
2018-02-20 08:10:56 -0800

    format updates

--- src/app/components/AssignmentDefaultRecurrenceDetails.tsx
+++ src/app/components/AssignmentDefaultRecurrenceDetails.tsx
@@ -1,8 +1,8 @@
 import * as React from 'react';
 import * as EnumUtils from '../infrastructure/EnumUtils';
-import { 
+import {
     RecurrenceInfo,
-    DaysOfWeek 
+    DaysOfWeek
 } from '../api';
 import DateDisplay from './DateDisplay';
 import { Badge } from 'react-bootstrap';
@@ -14,12 +14,12 @@ export interface AssignmentDefaultRecurrenceDetailsProps {
 
 export default class AssignmentDefaultRecurrenceDetails extends React.PureComponent<AssignmentDefaultRecurrenceDetailsProps, any> {
     render() {
-        const { data: {days, startTime, endTime, sheriffsRequired} } = this.props;
+        const { data: { days, startTime, endTime, sheriffsRequired } } = this.props;
         const dayDisplay = EnumUtils.displayEnum(DaysOfWeek, days).join(", ");
-        
+
         return (
             <div>
-                <strong>{dayDisplay}</strong> - <DateDisplay date={startTime} showTime /> to <DateDisplay date={endTime} showTime /> {' '} 
+                <strong>{dayDisplay}</strong> - <DateDisplay date={startTime} showTime /> to <DateDisplay date={endTime} showTime /> {' '}
                 <Badge>{sheriffsRequired}</Badge>
                 <br />
             </div>
