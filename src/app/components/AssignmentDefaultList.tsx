c6e05f4a6574d7b0dde8376d84c68394bab05ac4
holly.mcquay <holly.mcquay@CANL-C02VV2XJHTDD.local>
2018-02-21 14:32:30 -0800

    change order of columns for display

--- src/app/components/AssignmentDefaultList.tsx
+++ src/app/components/AssignmentDefaultList.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import {
-    Table, 
+    Table,
     Badge
 } from 'react-bootstrap';
 import AssignmentDefaultRecurrenceDetails from './AssignmentDefaultRecurrenceDetails';
@@ -17,7 +17,7 @@ export interface AssignmentDefaultListProps {
 
 export default class AssignmentDefaultList extends React.Component<AssignmentDefaultListProps> {
     render() {
-        const { assignments = []} = this.props;
+        const { assignments = [] } = this.props;
         return (
             <div>
                 {assignments.length == 0 && (
@@ -27,24 +27,24 @@ export default class AssignmentDefaultList extends React.Component<AssignmentDef
                     <Table responsive striped>
                         <thead>
                             <tr>
-                                <th className="text-left">Assignment</th>
                                 <th className="text-left">Work Section</th>
+                                <th className="text-left">Assignment</th>
                                 <th className="text-left">Default Days &amp; Times <Badge># of Sheriffs</Badge></th>
-                                
+
                             </tr>
                         </thead>
                         <tbody>
                             {assignments.map((assignment) => {
                                 return (
                                     <tr>
-                                        <td><strong>{assignment.title}</strong></td>
                                         <td>{WORK_SECTIONS[assignment.workSectionId]}</td>
+                                        <td><strong>{assignment.title}</strong></td>
                                         <td>
-                                            {assignment.recurrenceInfo && 
+                                            {assignment.recurrenceInfo &&
                                                 assignment.recurrenceInfo.map((recurrence) => <AssignmentDefaultRecurrenceDetails data={recurrence} />)}
                                         </td>
-                                        <td className="text-right"><AssignmentEditModal assignmentId={assignment.id}/></td> 
-                                        <td className="text-left"><AssignmentDeleteModal assignmentId={assignment.id}/></td>
+                                        <td className="text-right"><AssignmentEditModal assignmentId={assignment.id} /></td>
+                                        <td className="text-left"><AssignmentDeleteModal assignmentId={assignment.id} /></td>
                                     </tr>
                                 );
                             })}
