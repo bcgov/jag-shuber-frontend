6dbcd0561b9ad9fea6def0e32959e9b55869a688
holly.mcquay <holly.mcquay@CANL-C02VV2XJHTDD.local>
2018-02-27 08:08:59 -0800

    adding tests for DaysOfWeek dispplay

--- src/app/api/__tests__/ApiTest.ts
+++ src/app/api/__tests__/ApiTest.ts
@@ -26,16 +26,16 @@ describe('DaysOfWeek.getDisplayValues', () => {
     const util = DaysOfWeek.getDisplayValues;
     
     it('Should return each day if weekdays and Sat or Sun is selected', () => {
-        expect(util(DaysOfWeek.Weekdays && DaysOfWeek.Sat)).toEqual(["Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]);
-        expect(util(DaysOfWeek.Weekdays && DaysOfWeek.Sun)).toEqual(["Mon", "Tue", "Wed", "Thu", "Fri", "Sun"]);
+        expect(util(DaysOfWeek.Weekdays | DaysOfWeek.Sat)).toEqual(["Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]);
+        expect(util(DaysOfWeek.Weekdays | DaysOfWeek.Sun)).toEqual(["Mon", "Tue", "Wed", "Thu", "Fri", "Sun"]);
     }) 
     
     it('Should return each day if weekdays and Sat and Sun is selected', () => {
-        expect(util(DaysOfWeek.Weekdays && DaysOfWeek.Sat && DaysOfWeek.Sun)).toEqual(["Everyday"]);
+        expect(util(DaysOfWeek.Weekdays | DaysOfWeek.Sat | DaysOfWeek.Sun)).toEqual(["Everyday"]);
     })
 
     it('Should return each day if not all weekdays or everyday are selected', () => {
-        expect(util(DaysOfWeek.Mon && DaysOfWeek.Tue && DaysOfWeek.Sat && DaysOfWeek.Sun)).toEqual(["Mon", "Tue", "Sat", "Sun"]);
+        expect(util(DaysOfWeek.Mon | DaysOfWeek.Tue | DaysOfWeek.Sat | DaysOfWeek.Sun)).toEqual(["Mon", "Tue", "Sat", "Sun"]);
     })
 
 });
