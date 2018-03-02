4ab091be15e153f8aa7e78de89237788ff66bc70
holly.mcquay <holly.mcquay@CANL-C02VV2XJHTDD.local>
2018-03-02 13:38:20 -0800

    remove colour selection from work section indicator

--- src/app/components/WorkSectionIndicator/WorkSectionIndicator.tsx
+++ src/app/components/WorkSectionIndicator/WorkSectionIndicator.tsx
@@ -1,36 +1,19 @@
 import * as React from 'react';
 import './WorkSectionIndicator.css';
 import { WorkSectionId } from '../../api/Api';
+import {
+    getWorkSectionColour
+} from '../../api/utils';
 
 export interface WorkSectionIndicatorProps {
     workSectionId: WorkSectionId;
 }
 
 export default class WorkSectionIndicator extends React.PureComponent<WorkSectionIndicatorProps, any> {
-    private getColour(workSectionId: WorkSectionId): string {
-        let colour = 'blue';
-
-        switch (workSectionId) {
-            case "COURTS":
-            colour = '#2CB7BA';
-                break;
-            case "ESCORTS":
-                colour = '#F3BD48'
-                break;
-            case "JAIL":
-                colour = '#804A86';
-                break;
-            case "OTHER":
-                colour = "#B74343"
-                break;
-        }
-        return colour;
-    }
-
     render() {
         const { workSectionId = 'OTHER' } = this.props
         return (
-            <div className="work-section-indicator" style={{ borderTopColor: this.getColour(workSectionId) }}>
+            <div className="work-section-indicator" style={{ borderTopColor: getWorkSectionColour(workSectionId) }}>
             </div>
         );
     }
