b81337232360074aa41e670354d0adee28327fbe
holly.mcquay <holly.mcquay@CANL-C02VV2XJHTDD.local>
2018-03-19 11:38:24 -0700

    Adding style and fleshing out story book

---
+++ src/app/components/ScheduleSummary/ScheduleSummary.tsx
@@ -0,0 +1,70 @@
+import * as React from 'react';
+import './ScheduleSummary.css';
+import { Glyphicon } from 'react-bootstrap';
+
+export enum StatusEnum {
+    GOOD,
+    BAD,
+    WARNING,
+    EMPTY,
+    HIDDEN
+}
+
+export interface ScheduleSummaryProps {
+    weekStatus: {
+        sunday?: StatusEnum;
+        monday?: StatusEnum;
+        tuesday?: StatusEnum;
+        wednesday?: StatusEnum;
+        thursday?: StatusEnum;
+        friday?: StatusEnum;
+        saturday?: StatusEnum;
+    };
+}
+
+export default class ScheduleSummary extends React.PureComponent<ScheduleSummaryProps, {}> {
+    getDayInfo(status: StatusEnum, day: string) {
+        switch (status) {
+            case StatusEnum.GOOD:
+                return (
+                    <div className="schedule-summary-day good-day non-empty-day">
+                        <Glyphicon glyph="ok"/>
+                    </div>
+                );
+            case StatusEnum.BAD:
+                return (
+                    <div className="schedule-summary-day bad-day non-empty-day">
+                        <Glyphicon glyph="remove"/>
+                    </div>
+                );
+            case StatusEnum.WARNING:
+                return  (
+                    <div className="schedule-summary-day warning-day non-empty-day">
+                        <Glyphicon glyph="warning-sign"/>
+                    </div>
+                );
+            default:
+               return (
+                    <div className="schedule-summary-day empty-day">
+                        {day.charAt(0).toUpperCase()}
+                    </div>
+                );
+        }
+    }
+
+    render() {
+
+        const { weekStatus } = this.props;
+        const dayStatus = Object.keys(weekStatus).map(key => ({ key, status: weekStatus[key] }));
+
+        return (
+
+            <div className="schedule-summary-week" >
+                {dayStatus.filter(({ status }) => status !== StatusEnum.HIDDEN).map(({ status, key }) => (
+                    this.getDayInfo(status, key)
+                ))}
+            </div>
+
+        );
+    }
+}
--- src/stories/Components/ScheduleSummary.stories.tsx
+++ src/stories/Components/ScheduleSummary.stories.tsx
@@ -1,15 +1,86 @@
 import * as React from 'react';
 import { storiesOf } from '../utils';
-// import { text } from '@storybook/addon-knobs';
 import StoryPage from '../StoryUI/StoryPage';
 import StorySection from '../StoryUI/StorySection';
-import ScheduleSummary from '../../app/components/ScheduleSummary';
+import {
+  default as ScheduleSummary,
+  StatusEnum
+} from '../../app/components/ScheduleSummary/ScheduleSummary';
+import '../../app/assets/styles/Glyphicons.css';
 
 storiesOf('Components')
   .add('Schedule Summary', () => (
     <StoryPage title="Schedule Summary">
-      <StorySection title="Default / Empty">
-        <ScheduleSummary />
+
+      <StorySection title="Fully Schedulued">
+        <ScheduleSummary
+          weekStatus={{
+            sunday: StatusEnum.HIDDEN,
+            monday: StatusEnum.GOOD,
+            tuesday: StatusEnum.GOOD,
+            wednesday: StatusEnum.GOOD,
+            thursday: StatusEnum.GOOD,
+            friday: StatusEnum.GOOD,
+            saturday: StatusEnum.HIDDEN
+          }}
+        />
       </StorySection>
+
+      <StorySection title="Fully Available">
+        <ScheduleSummary
+          weekStatus={{
+            sunday: StatusEnum.HIDDEN,
+            monday: StatusEnum.EMPTY,
+            tuesday: StatusEnum.EMPTY,
+            wednesday: StatusEnum.EMPTY,
+            thursday: StatusEnum.EMPTY,
+            friday: StatusEnum.EMPTY,
+            saturday: StatusEnum.HIDDEN
+          }}
+        />
+      </StorySection>
+
+      <StorySection title="Available with Warnings">
+        <ScheduleSummary
+          weekStatus={{
+            sunday: StatusEnum.HIDDEN,
+            monday: StatusEnum.EMPTY,
+            tuesday: StatusEnum.EMPTY,
+            wednesday: StatusEnum.EMPTY,
+            thursday: StatusEnum.WARNING,
+            friday: StatusEnum.EMPTY,
+            saturday: StatusEnum.HIDDEN
+          }}
+        />
+      </StorySection>
+
+      <StorySection title="Available with Days Off">
+        <ScheduleSummary
+          weekStatus={{
+            sunday: StatusEnum.HIDDEN,
+            monday: StatusEnum.EMPTY,
+            tuesday: StatusEnum.EMPTY,
+            wednesday: StatusEnum.EMPTY,
+            thursday: StatusEnum.BAD,
+            friday: StatusEnum.BAD,
+            saturday: StatusEnum.HIDDEN
+          }}
+        />
+      </StorySection>
+      
+      <StorySection title="Fully Schedulued - Weekends Included">
+        <ScheduleSummary
+          weekStatus={{
+            sunday: StatusEnum.EMPTY,
+            monday: StatusEnum.GOOD,
+            tuesday: StatusEnum.GOOD,
+            wednesday: StatusEnum.GOOD,
+            thursday: StatusEnum.GOOD,
+            friday: StatusEnum.GOOD,
+            saturday: StatusEnum.EMPTY
+          }}
+        />
+      </StorySection>
+
     </StoryPage>
   ));
