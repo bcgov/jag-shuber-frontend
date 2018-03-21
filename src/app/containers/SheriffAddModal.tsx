023e5d86a0f194bea46f3b876a4b301444226ea6
holly.mcquay <holly.mcquay@CANL-C02VV2XJHTDD.local>
2018-03-21 16:00:53 -0700

    Style Updates

--- src/app/containers/SheriffAddModal.tsx
+++ src/app/containers/SheriffAddModal.tsx
@@ -9,8 +9,11 @@ export default class SheriffAddModal extends React.Component {
     render() {
         return (
             <ModalWrapper
-                title="Edit Assignment"
-                showButton={({ handleShow }) => <Button bsStyle='primary' onClick={() => handleShow()}>Add a Sheriff</Button>}
+                title="Add Sheriff"
+                showButton={({ handleShow }) => 
+                    <Button bsStyle="primary" onClick={() => handleShow()}>
+                        Add a Sheriff
+                    </Button>}
                 body={({ handleClose }) => <SheriffCreateForm onSubmitSuccess={handleClose} />}
                 footerComponent={<SheriffCreateForm.SubmitButton>Save</SheriffCreateForm.SubmitButton>}
             />
