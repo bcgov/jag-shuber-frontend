e89a2cda69ef2c964abe2dfeb70baae03e54c85a
holly.mcquay <holly.mcquay@CANL-C02VV2XJHTDD.local>
2018-03-22 16:31:12 -0700

    Adding new validator for integer between 1 and 50

--- src/app/infrastructure/Validators.ts
+++ src/app/infrastructure/Validators.ts
@@ -1,40 +1,38 @@
 export const VALIDATOR_MESSAGES = {
-    INVALID_INTEGER: "Must be an integer.",
-    INVALID_NUMBER: "Must be a number.",
-    REQUIRED_VALUE: "This is a required field."
-    
-}
+    INVALID_INTEGER: 'Must be an integer.',
+    INVALID_NUMBER: 'Must be a number.',
+    REQUIRED_VALUE: 'This is a required field.',
+    INVALID_INTEGER_RANGE: 'Must be an integer between 1 and 50'   
+};
 
-export const required = (value?:any) => value || value===false ? undefined :  VALIDATOR_MESSAGES.REQUIRED_VALUE;
+export const required = (value?: any) => value || value === false ? undefined :  VALIDATOR_MESSAGES.REQUIRED_VALUE;
 
-
-export const integer = (value:any) =>{ 
+export const integer = (value: any) => {  
     let invalidMessage = number(value);
-    if(!invalidMessage){
+    if (!invalidMessage) {
         const numericValue = Number(value);
-        if(Math.floor(numericValue)===numericValue){
+        if (Math.floor(numericValue) === numericValue) {
             invalidMessage = undefined;
-        }else{
+        } else {
              invalidMessage = VALIDATOR_MESSAGES.INVALID_INTEGER;
         }
     }
     return invalidMessage;
-}
+};
 
-export const number = (value:any) =>{ 
+export const integerBetween1and50 = (value: any) => {
+    let invalidMessage = integer(value);
+    if (!invalidMessage) {
+        const numericValue = Number(value);
+        if (numericValue <= 50 && numericValue >= 1) {
+            invalidMessage = undefined;
+        } else {
+            invalidMessage = VALIDATOR_MESSAGES.INVALID_INTEGER_RANGE;
+        }
+    }
+    return invalidMessage;
+};
+
+export const number = (value: any) => { 
     return value && isNaN(value) ? VALIDATOR_MESSAGES.INVALID_NUMBER : undefined;
-}
-
-/* examples of other validators that can be used as needed
-
-const minValue = min => value =>
-  value && value < min ? `Must be at least ${min}` : undefined
-const minValue18 = minValue(18)
-const email = value =>
-  value && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value) ?
-  'Invalid email address' : undefined
-const tooOld = value =>
-  value && value > 65 ? 'You might be too old for this' : undefined
-const aol = value =>
-  value && /.+@aol\.com/.test(value) ?
-  'Really? You still use AOL for your email?' : undefined */
+};
