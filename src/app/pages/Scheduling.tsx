dc7d51c2b9efccea57d1bc392c50c2c41423e792
holly.mcquay <holly.mcquay@CANL-C02VV2XJHTDD.local>
2018-03-26 14:24:37 -0700

    Style updates to display of scheduling toolbar

--- src/app/pages/Scheduling.tsx
+++ src/app/pages/Scheduling.tsx
@@ -9,55 +9,32 @@ import SheriffDragSource from '../containers/SheriffDragSource';
 import ScheduleShiftAddModal from '../containers/ScheduleShiftAddModal';
 import ScheduleShiftCopyModal from '../containers/ScheduleShiftCopyModal';
 import './pages.css';
-// import { Glyphicon, 
-//     Button, 
-//     // DropdownButton, 
-//     // Dropdown, 
-//     // MenuItem 
-// } from 'react-bootstrap';
 
 class SchedulingPage extends React.PureComponent {
     render() {
         return (
             <div>
-                <div className="scheduling-toolbar">
-                {/* <Dropdown id="dropdown-custom-1">
-                        <Dropdown.Toggle noCaret bsStyle="link" bsSize="large" style={{color: 'white'}}>
-                        <Glyphicon glyph="wrench" />
-                        </Dropdown.Toggle>
-                        <Dropdown.Menu>
-                        <MenuItem eventKey="1"><ScheduleShiftAddModal /></MenuItem>
-                        <MenuItem eventKey="2"><Glyphicon style={{color: 'blue'}} glyph="repeat"/></MenuItem>
-                        </Dropdown.Menu>
-                    </Dropdown>
-
-                    <DropdownButton title="test" id="testing" noCaret>
-                        <MenuItem> <ScheduleShiftAddModal /></MenuItem>
-                        <MenuItem> <Glyphicon style={{color: 'blue'}} glyph="repeat"/></MenuItem>
-                        </DropdownButton> */}
-
-
+                <div className="scheduling-toolbar" style={{ display: 'flex', flexDirection: 'row-reverse' }}>
                     <ScheduleShiftAddModal />
                     <ScheduleShiftCopyModal />
                 </div>
-            <div style={{ display: 'flex', flexDirection: 'row'}}>
-                
-                <TimelineToolsPanel titleText="My Team">
-                    <ListGroup>
-                        <SheriffList
-                            SheriffRenderer={(s: Sheriff) => (
-                                <SheriffDragSource sheriff={s}>
-                                    <SheriffListCard sheriff={s} showScheduleSummary={true} />
-                                </SheriffDragSource>
-                            )}
-                        />
-                    </ListGroup>
-                </TimelineToolsPanel>
-                <div style={{ flex: '1 0px', minWidth: 500 }}>
-                    <SchedulingTimeline />
+                <div style={{ display: 'flex', flexDirection: 'row' }}>
+                    <TimelineToolsPanel titleText="My Team">
+                        <ListGroup>
+                            <SheriffList
+                                SheriffRenderer={(s: Sheriff) => (
+                                    <SheriffDragSource sheriff={s}>
+                                        <SheriffListCard sheriff={s} showScheduleSummary={true} />
+                                    </SheriffDragSource>
+                                )}
+                            />
+                        </ListGroup>
+                    </TimelineToolsPanel>
+                    <div style={{ flex: '1 0px', minWidth: 500 }}>
+                        <SchedulingTimeline />
+                    </div>
                 </div>
-            </div>
-            
+
             </div>
         );
     }
