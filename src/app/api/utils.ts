081bd4cb2cf4329d7e64e12fa9e0b64d0450cdb1
holly.mcquay <holly.mcquay@CANL-C02VV2XJHTDD.local>
2018-03-27 12:26:18 -0700

    re-configure shift form to be useable for shift edit and shift creation

--- src/app/api/utils.ts
+++ src/app/api/utils.ts
@@ -14,7 +14,6 @@ import {
 export type ShiftCreationPayload = {
     weekStart: DateType;
     workSectionId?: WorkSectionId;
-    workSectionNotRequired?: boolean;
     startTime: DateType;
     endTime: DateType;
     days: DaysOfWeek;
--- src/app/components/ScheduleShiftForm.tsx
+++ src/app/components/ScheduleShiftForm.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import * as moment from 'moment';
 import {
     Form
 } from 'react-bootstrap';
@@ -10,59 +9,27 @@ import {
 import * as Validators from '../infrastructure/Validators';
 import * as DateTimeFieldConst from './FormElements/DateTimeFieldConst';
 import {
-    DateType,
-    DaysOfWeek
+    DateType
 } from '../api/Api';
 import DaysOfWeekChecklist from './FormElements/DaysOfWeekChecklist';
 import WorkSectionSelector from './FormElements/WorkSectionSelector';
 import NumberSpinner from './FormElements/NumberSpinner';
-import CheckboxField from './FormElements/CheckboxField';
 
 export interface ScheduleShiftFormProps {
     handleSubmit?: () => void;
     onSubmitSuccess?: () => void;
-    weekStart: DateType;
+    weekStart?: DateType;
+    isSingleShift?: boolean;
 }
 
 export default class ScheduleShiftForm extends
     React.Component<ScheduleShiftFormProps & InjectedFormProps<{}, ScheduleShiftFormProps>, {}> {
 
-    public static createDefaultShift() {
-        return {
-            startTime: moment().hour(9).minute(0),
-            endTime: moment().hour(17).minute(0),
-            days: DaysOfWeek.Weekdays,
-            repeatNumber: 1
-        };
-    }
-
-    render() {
-        const { handleSubmit } = this.props;
-        return (
-            <div>
-                <Form onSubmit={handleSubmit}>
-                    <Field
-                        name="workSectionId"
-                        component={WorkSectionSelector}
-                        label="Work Section"
-                    />
-                    <Field
-                        name="workSectionNotRequired"
-                        component={CheckboxField}
-                        label="Work Section Not Applicable"
-                    />
-                    <Field
-                        name="startTime"
-                        component={DateTimeFieldConst.TimeField}
-                        label="Start Time"
-                        validate={[Validators.required]}
-                    />
-                    <Field
-                        name="endTime"
-                        component={DateTimeFieldConst.TimeField}
-                        label="End Time"
-                        validate={[Validators.required]}
-                    />
+    private renderMultiShiftCreationFields() {
+        const { isSingleShift } = this.props;
+        if (!isSingleShift) {
+            return (
+                <div>
                     <Field
                         name="days"
                         component={DaysOfWeekChecklist}
@@ -80,6 +47,44 @@ export default class ScheduleShiftForm extends
                             Validators.integer
                         ]}
                     />
+                </div>
+            );
+        }
+        return '';
+    }
+
+    private renderShiftFields() {
+        const { isSingleShift } = this.props;
+        return (
+            <div>
+                <Field
+                    name="workSectionId"
+                    component={WorkSectionSelector}
+                    label="Work Section"
+                />
+                <Field
+                    name={isSingleShift ? 'startDateTime' : 'startTime'}
+                    component={DateTimeFieldConst.TimeField}
+                    label="Start Time"
+                    validate={[Validators.required]}
+                />
+                <Field
+                    name={isSingleShift ? 'endDateTime' : 'endTime'}
+                    component={DateTimeFieldConst.TimeField}
+                    label="End Time"
+                    validate={[Validators.required]}
+                />
+            </div>
+        );
+    }
+
+    render() {
+        const { handleSubmit } = this.props;
+        return (
+            <div>
+                <Form onSubmit={handleSubmit}>
+                    {this.renderShiftFields()}
+                    {this.renderMultiShiftCreationFields()}
                 </Form>
             </div>
         );
