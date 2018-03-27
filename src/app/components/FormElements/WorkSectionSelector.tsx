4f946844ed4e52031de3e362a90e9785a17f6af0
holly.mcquay <holly.mcquay@CANL-C02VV2XJHTDD.local>
2018-03-27 12:25:21 -0700

    Adding 'not applicable' option to work section selector

--- src/app/components/FormElements/WorkSectionSelector.tsx
+++ src/app/components/FormElements/WorkSectionSelector.tsx
@@ -3,9 +3,10 @@ import { FormFieldWrapperProps } from './FormFieldWrapper';
 import { WORK_SECTIONS } from '../../api';
 import Selector from './Selector';
 
-export default class WorkSectionSelector extends React.PureComponent<FormFieldWrapperProps>{
+export default class WorkSectionSelector extends React.PureComponent<FormFieldWrapperProps> {
     render() {        
-        const data = Object.keys(WORK_SECTIONS).map((key, index) => ({key, value: WORK_SECTIONS[key]}));
+        let data = Object.keys(WORK_SECTIONS).map((key, index) => ({key, value: WORK_SECTIONS[key]}));
+        data.push({key: 'NA', value: 'Not Applicable'});
         return (
             <Selector data={data} {...this.props}/>
         );
