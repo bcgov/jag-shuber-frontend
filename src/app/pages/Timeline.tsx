652248211489b1fd417eb084547a63097ca79bb8
holly.mcquay <holly.mcquay@CANL-C02VV2XJHTDD.local>
2018-04-11 09:24:43 -0700

    removing commented line

--- src/app/pages/Timeline.tsx
+++ src/app/pages/Timeline.tsx
@@ -4,7 +4,6 @@ import DailyTimeline from '../containers/DailyTimeline/DailyTimeline';
 import TimelineToolsPanel from '../components/TimelineToolsPanel';
 import SheriffList from '../containers/SheriffList';
 import { Sheriff } from '../api';
-// import SheriffListCard from '../components/SheriffListCard/SheriffListCard';
 import { ListGroup } from 'react-bootstrap';
 import SheriffDragSource from '../containers/SheriffDragSource';
 import DutyRosterSheriffCard from '../containers/DutyRosterSheriffCard';
