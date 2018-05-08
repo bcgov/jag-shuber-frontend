1579c69d043d6b6f7210f583b50561024157f21a
holly.mcquay <holly.mcquay@CANL-C02VV2XJHTDD.local>
2018-05-08 13:56:46 -0700

    renaing unused components

---
+++ src/app/components/X_SheriffLocationDetails.tsx
@@ -0,0 +1,38 @@
+// import * as React from 'react';
+// import {
+//     Table
+// } from 'react-bootstrap';
+// import CourthouseCard from './CourthouseCard';
+
+// export interface SheriffLocationDetailsProps {
+
+// }
+
+// export default class SheriffLocationDetails extends React.Component<SheriffLocationDetailsProps, any>{
+//     render() {
+//         // const { currentLocation = BLANK_SHERIFF_LOCATION, permanentLocation = BLANK_SHERIFF_LOCATION } = this.props;
+//         return (
+//             <div>
+//                 <h3>Location</h3>
+//                 <Table responsive={true} >
+//                     <thead>
+//                         <tr>
+//                             <th/>
+//                             <th className="text-left">Courthouse</th>
+//                         </tr>
+//                     </thead>
+//                     <tbody>
+//                         <tr>
+//                             <td><strong>Permanent</strong></td>
+//                             <td><CourthouseCard /></td>
+//                         </tr>
+//                         <tr>
+//                             <td><strong>Current</strong></td>
+//                             <td><CourthouseCard /></td>
+//                         </tr>
+//                     </tbody>
+//                 </Table>
+//             </div>
+//         );
+//     }
+// }
---
+++ src/app/components/X_SheriffUpcomingScheduleDetails.tsx
@@ -0,0 +1,59 @@
+// import * as React from 'react';
+// import { 
+//     Table
+// } from 'react-bootstrap';
+// // import SheriffDutyStatus from './SheriffDutyStatus';
+
+// export interface SheriffUpcomingScheduleDetailsProps {
+//     onDuty: boolean;
+// }
+
+// export default class SheriffUpcomingScheduleDetails extends React.Component<SheriffUpcomingScheduleDetailsProps, any>{
+//     render() {
+//         // const { onDuty } = this.props;
+//         return (
+//             <div>
+//                 {/* <SheriffDutyStatus onDuty ={ onDuty } /> */}
+                
+//                 <Table responsive>
+//                     <thead>
+//                         <tr>
+//                             <th className="text-left">Day</th>
+//                             <th className="text-left">Shift</th>
+//                         </tr>
+//                     </thead>
+//                     <tbody>
+//                         <tr>
+//                             <td>Sun Jan 28 2018</td>
+//                             <td>7AM to 7PM </td>
+//                         </tr>
+//                         <tr>
+//                             <td>Mon Jan 29 2018</td>
+//                             <td>7AM to 7PM </td>
+//                         </tr>
+//                         <tr>
+//                             <td>Tues Jan 30 2018</td>
+//                             <td>7AM to 7PM </td>
+//                         </tr>
+//                         <tr>
+//                             <td>Wed Jan 31 2018</td>
+//                             <td>7AM to 7PM </td>
+//                         </tr>
+//                         <tr>
+//                             <td>Thur Feb 01 2018</td>
+//                             <td>7AM to 7PM </td>
+//                         </tr>
+//                         <tr>
+//                             <td>Fri Feb 02 2018</td>
+//                             <td>7AM to 7PM </td>
+//                         </tr>
+//                         <tr>
+//                             <td>Sat Feb 03 2018</td>
+//                             <td>7AM to 7PM </td>
+//                         </tr>
+//                     </tbody>
+//                 </Table>
+//             </div>
+//         );
+//     }
+// }
