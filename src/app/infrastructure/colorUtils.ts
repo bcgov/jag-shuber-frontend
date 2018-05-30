9f42e463f2b5674bc07dd5984553ecef8226423c
holly.mcquay <holly.mcquay@CANL-C02VV2XJHTDD.local>
2018-05-30 08:59:48 -0700

    changing color calculation for text on green

--- src/app/infrastructure/colorUtils.ts
+++ src/app/infrastructure/colorUtils.ts
@@ -6,7 +6,7 @@ export function getForegroundColor(backgroundColor: string): string {
 
     const color = Color(backgroundColor);
     // Counting the perceptive luminance - human eye favors green color... 
-    const perceivedEnergy: number = 1 - (0.299 * color.red() + 0.587 * color.green() + 0.114 * color.blue()) / 255;
+    const perceivedEnergy: number = 1 - (0.299 * color.red() + 0.400 * color.green() + 0.114 * color.blue()) / 255;
 
     if (perceivedEnergy < 0.5) {
         return '#000000'; // bright colors - black font
