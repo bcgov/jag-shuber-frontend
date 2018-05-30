ec07aa60c3d0b38c15349ade6a98c03260b19683
Colter McQuay <colter.mcquay@roughdraftdev.com>
2018-05-30 13:41:25 -0700

    Sorting worksection selector

--- src/app/components/FormElements/WorkSectionSelector.tsx
+++ src/app/components/FormElements/WorkSectionSelector.tsx
@@ -9,7 +9,9 @@ export interface WorkSectionSelectorProps {
 export default class WorkSectionSelector extends React.PureComponent<FormFieldWrapperProps & WorkSectionSelectorProps> {
     render() {
         const { showVariedOption = false } = this.props;
-        let data = Object.keys(WORK_SECTIONS).map((key, index) => ({ key, value: WORK_SECTIONS[key] }));
+        let data = Object.keys(WORK_SECTIONS)
+            .map((key, index) => ({ key, value: WORK_SECTIONS[key] }))
+            .sort((a, b) => a.value.localeCompare(b.value));
         return (
             <Selector
                 data={data}
