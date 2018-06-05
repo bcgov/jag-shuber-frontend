8e697585403fbf4e6ada8a383fd88527b6e4f61c
holly.mcquay <holly.mcquay@CANL-C02VV2XJHTDD.local>
2018-06-05 14:40:43 -0700

    Adding loan icon to duty roster sheriff card

--- src/app/components/Icons/SheriffLoanOutIcon.tsx
+++ src/app/components/Icons/SheriffLoanOutIcon.tsx
@@ -2,12 +2,12 @@ import * as React from 'react';
 import { loadIcon } from '../../infrastructure/openIconicUtils';
 const Icon = loadIcon('account-logout');
 
-export interface SheriffLoanInIconProps {
+export interface SheriffLoanOutIconProps {
     width?: number;
     height?: number;
     color?: string;
 }
-export default class SheriffLoanInIcon extends React.PureComponent<SheriffLoanInIconProps> {
+export default class SheriffLoanOutIcon extends React.PureComponent<SheriffLoanOutIconProps> {
     render() {
         const { width = 23, height = 23, color: fill = '#808080' } = this.props;
         return (
