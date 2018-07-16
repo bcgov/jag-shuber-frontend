eba7eec190476c22eef36d7d794560872347a4d4
Colter McQuay <colter.mcquay@roughdraftdev.com>
2018-07-16 13:03:50 -0700

    Fixing some oc scripts

--- openshift/create-oc-frontend-builds.sh
+++ openshift/create-oc-frontend-builds.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
+SCRIPT_DIR=`dirname $0`
 echo "You should log into OpenShift and select your project before running this script."
 
 read -p "Continue? (Y/n): " ok
 ok=${ok:-y}
 ok=$(echo $ok |awk '{print tolower($0)}')
 
-#params="-p GIT_REF=chained-build+slack"
-params=""
+params="-p GIT_REF=site-minder-support"
+#params=""
 
 if [ "$ok" == "y" ]; then
     echo "Deploying frontend builds environment..."
     echo "Deploying nginx runtime build..."
-    oc process -f "./templates/nginx-runtime/nginx-runtime-build.json" $params | oc create -f -
+    oc process -f "$SCRIPT_DIR/templates/nginx-runtime/nginx-runtime-build.json" $params | oc create -f -
     echo "ok \nDeploying yarn builder build..."
-    oc process -f "./templates/yarn-builder/yarn-builder-build.json" $params | oc create -f -
+    oc process -f "$SCRIPT_DIR/templates/yarn-builder/yarn-builder-build.json" $params | oc create -f -
     echo "ok \nDeploying frontend build..."
-    oc process -f "./templates/frontend/frontend-build.json" $params | oc create -f -
+    oc process -f "$SCRIPT_DIR/templates/frontend/frontend-build.json" $params | oc create -f -
 else
     exit 0
 fi
--- openshift/deploy-oc-frontend.sh
+++ openshift/deploy-oc-frontend.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+SCRIPT_DIR=`dirname $0`
 echo "You should log into OpenShift and select your project before running this script."
 
 read -p "Continue? (Y/n): " ok
@@ -10,7 +11,7 @@ params="-p IMAGE_NAMESPACE=tools"
 
 if [ "$ok" == "y" ]; then
     echo "Deploying frontend application"
-    oc process -f "./templates/frontend/frontend-deploy.json" $params | oc create -f -
+    oc process -f "$SCRIPT_DIR/templates/frontend/frontend-deploy.json" $params | oc create -f -
 else
     exit 0
 fi
