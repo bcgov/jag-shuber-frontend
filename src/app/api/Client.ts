5cf9678bab667374fce536d22caa4197f190f6cb
holly.mcquay <holly.mcquay@CANL-C02VV2XJHTDD.local>
2018-08-16 10:04:59 -0700

    updating re-assignment logic

--- src/app/api/Client.ts
+++ src/app/api/Client.ts
@@ -216,6 +216,8 @@ export default class Client implements API {
             targetSheriffDuty
         } = reassignmentDetails;
 
+        const sheriffDutyPromises: Promise<SheriffDuty>[] = [];
+
         // Source Sheriff Duty
         const sourceEndTimeMoment = moment(newSourceDutyEndTime);
         const sourceCutOffTime = moment(sourceSheriffDuty.startDateTime)
@@ -223,18 +225,27 @@ export default class Client implements API {
             .minutes(sourceEndTimeMoment.minute())
             .toISOString();
 
-        // Create a new sheriff duty to account for the remaining/uncoverd time in the source sheriff duty
-        const remainingSourceDuty = this.createSheriffDuty({
-            dutyId: sourceSheriffDuty.dutyId,
-            startDateTime: sourceCutOffTime,
-            endDateTime: moment(sourceSheriffDuty.endDateTime).toISOString()
-        });
+        if (moment(sourceCutOffTime).isSame(moment(sourceSheriffDuty.startDateTime), 'minute')) {
+            // Remove the sheriff from the source duty
+            sheriffDutyPromises.push(this.updateSheriffDuty({
+                ...sourceSheriffDuty,
+                sheriffId: undefined
+            }));
+        } else if (!moment(sourceCutOffTime).isSame(moment(sourceSheriffDuty.endDateTime), 'minute')) {
+            // Create a new sheriff duty to account for the remaining/uncoverd time in the source sheriff duty
+            sheriffDutyPromises.push(this.createSheriffDuty({
+                dutyId: sourceSheriffDuty.dutyId,
+                startDateTime: sourceCutOffTime,
+                endDateTime: moment(sourceSheriffDuty.endDateTime).toISOString()
+            }));
 
-        // End the source sheriff duty at the new source end time
-        const updatedSourceSheriffDuty = this.updateSheriffDuty({
-            ...sourceSheriffDuty,
-            endDateTime: sourceCutOffTime,
-        });
+            // End the source sheriff duty at the new source end time
+            sheriffDutyPromises.push(this.updateSheriffDuty({
+                ...sourceSheriffDuty,
+                endDateTime: sourceCutOffTime,
+            }));
+
+        }
 
         // Target Sheriff Duty
         const targetStartTimeMoment = moment(newTargetDutyStartTime);
@@ -243,29 +254,32 @@ export default class Client implements API {
             .minutes(targetStartTimeMoment.minute())
             .toISOString();
 
-        // Create a new sheriff duty to account for the time the source sheriff will spend in the target duty
-        const splitTargetSheriffDuty = this.createSheriffDuty({
-            dutyId: targetSheriffDuty.dutyId,
-            startDateTime: targetCutOffTime,
-            endDateTime: moment(targetSheriffDuty.endDateTime).toISOString(),
-            sheriffId: sourceSheriffDuty.sheriffId
-        });
-
-        let updatedTargetSheriffDuty = { ...targetSheriffDuty };
-        if (!targetSheriffDuty.sheriffId) {
-            // End the target sheriff duty at the new target start time
-            updatedTargetSheriffDuty = await this.updateSheriffDuty({
+        if (moment(targetCutOffTime).isSame(moment(targetSheriffDuty.startDateTime), 'minute')
+            && !targetSheriffDuty.sheriffId) {
+            // Assign source sheriff to exisitng target sheriff duty
+            sheriffDutyPromises.push(this.updateSheriffDuty({
                 ...targetSheriffDuty,
-                endDateTime: targetCutOffTime,
-            });
-        }             
+                sheriffId: sourceSheriffDuty.sheriffId
+            }));
+        } else {
+            // Create a new sheriff duty to account for the time the source sheriff will spend in the target duty
+            sheriffDutyPromises.push(this.createSheriffDuty({
+                dutyId: targetSheriffDuty.dutyId,
+                startDateTime: targetCutOffTime,
+                endDateTime: moment(targetSheriffDuty.endDateTime).toISOString(),
+                sheriffId: sourceSheriffDuty.sheriffId
+            }));
 
-        return Promise.all([
-            remainingSourceDuty,
-            updatedSourceSheriffDuty,
-            splitTargetSheriffDuty,
-            updatedTargetSheriffDuty
-        ]);
+            if (!targetSheriffDuty.sheriffId) {
+                //End the target sheriff duty at the new target start time
+                sheriffDutyPromises.push(this.updateSheriffDuty({
+                    ...targetSheriffDuty,
+                    endDateTime: targetCutOffTime,
+                }));
+            }
+        }
+
+        return Promise.all(sheriffDutyPromises);
     }
 
     async createDefaultDuties(date: moment.Moment = moment()): Promise<AssignmentDuty[]> {
