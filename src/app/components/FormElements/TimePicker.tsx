8fdf87a7680304078e2f61a8d47700f3049dc96b
Colter McQuay <colter.mcquay@roughdraftdev.com>
2018-08-16 17:33:23 -0700

    Initial work on the disabled slider sections

--- src/app/components/FormElements/TimePicker.tsx
+++ src/app/components/FormElements/TimePicker.tsx
@@ -2,18 +2,27 @@ import React from 'react';
 import moment from 'moment';
 import Slider from 'rc-slider';
 import { TimeType } from '../../api/Api';
+import {
+  HandleWithTooltip,
+  createMarks,
+  DisabledSliderSection,
+  adjustMarks,
+  SliderMarkCollection
+} from './TimeSliderCommon';
 import 'rc-slider/assets/index.css';
-import { HandleWithTooltip, createMarks } from './TimeSliderCommon';
 
 export interface TimePickerProps {
   minTime: TimeType;
   maxTime: TimeType;
+  minAllowedTime?: TimeType;
+  maxAllowedTime?: TimeType;
   selectedTime?: TimeType;
   timeIncrement?: number;
   onTimeChanged?: (selectedTime: TimeType) => void;
   handleColor?: string;
   railColor?: string;
 }
+
 export default class TimePicker extends React.Component<TimePickerProps> {
 
   private handleAfterChange(selectedMinutes: any) {
@@ -43,7 +52,6 @@ export default class TimePicker extends React.Component<TimePickerProps> {
         }
       }
     }
-
   }
 
   render() {
@@ -57,44 +65,136 @@ export default class TimePicker extends React.Component<TimePickerProps> {
     } = this.props;
     const minTime = moment(_minTime);
     const maxTime = moment(_maxTime);
-    const selectedTime = _selectedTime ? moment(_selectedTime) : minTime;
 
-    const durationMinutes: number = moment.duration(maxTime.diff(minTime)).asMinutes();
+    const minAllowedTime = moment(minTime).add(2, 'hours');
+    const maxAllowedTime = moment(maxTime).subtract(2, 'hours');
+
+    // if selected time isn't set, use either the minimum allowed time or the minTime for the slider
+    const selectedTime = _selectedTime ? moment(_selectedTime) : (minAllowedTime ? moment(minAllowedTime) : minTime);
     const defaultSelectedMin = selectedTime ? moment.duration(selectedTime.diff(minTime)).asMinutes() : 0;
 
-    const markLabels = createMarks(moment(minTime), moment(maxTime), timeIncrement);
+    type SliderSectionProps = {
+      width?: any;
+      start: number;
+      end: number;
+      marks: any;
+    };
+    let disabledMinSection: SliderSectionProps | undefined = undefined;
+    let disabledMaxSection: SliderSectionProps | undefined = undefined;
+
+    const fullDuration = moment.duration(maxTime.diff(minTime)).asMinutes()
+    const mainSection: SliderSectionProps = {
+      start: 0,
+      end: fullDuration,
+      marks: undefined
+    };
+
+    if (minAllowedTime) {
+      disabledMinSection = {
+        start: mainSection.start,
+        end: moment.duration(moment(minAllowedTime).diff(minTime)).asMinutes(),
+        marks: createMarks(moment(minTime), moment(minAllowedTime), timeIncrement)
+      };
+
+      // Blank the label on the last mark of the disabled timeline since we have that mark
+      // in the main section
+      const sortedKeys = Object.keys(disabledMinSection.marks)
+        .map(k => parseFloat(k))
+        .sort((a, b) => a - b);
+      const lastKey = sortedKeys[sortedKeys.length - 1];
+
+      (disabledMinSection.marks as SliderMarkCollection)[lastKey].label = '';
+
+      // Replace the main sections start with the end of the disabledMaxSection
+      mainSection.start = disabledMinSection.end;
+    }
+
+    if (maxAllowedTime) {
+      disabledMaxSection = {
+        start: moment.duration(moment(maxAllowedTime).diff(minTime)).asMinutes(),
+        end: mainSection.end,
+        marks: undefined
+      };
+
+      disabledMaxSection.marks = adjustMarks(
+        createMarks(moment(maxAllowedTime), moment(maxTime), timeIncrement),
+        disabledMaxSection.start
+      );
+
+      // Blank the label on the first mark of the disabled timeline since we have that mark
+      // in the main section
+      const firstKey = Object.keys(disabledMaxSection.marks)
+        .map(k => parseFloat(k))
+        .sort((a, b) => a - b)[0];
+      (disabledMaxSection.marks as SliderMarkCollection)[firstKey].label = '';
+
+      // Replace the main sections end with the start of the disabledMaxSection
+      mainSection.end = disabledMaxSection.start;
+    }
+    const mainMarks = createMarks(
+      moment(minTime).add(mainSection.start, 'minutes'),
+      moment(minTime).add(mainSection.end, 'minutes'),
+      timeIncrement);
+
+    // Need to adjust the mainSectionMarks to account for disabledMinSection
+    mainSection.marks = minAllowedTime ? adjustMarks(mainMarks, mainSection.start) : mainMarks;
 
     return (
       <div
         style={{
+          display: 'flex',
           marginLeft: 5,
           marginRight: 15,
           marginBottom: 25,
           marginTop: 15
         }}
       >
+        {disabledMinSection &&
+          <DisabledSliderSection
+            fullDuration={fullDuration}
+            step={timeIncrement}
+            dots={true}
+            min={disabledMinSection.start}
+            max={disabledMinSection.end}
+            marks={disabledMinSection.marks}
+          />
+        }
+
         <Slider
           step={timeIncrement}
           dots={true}
           defaultValue={defaultSelectedMin}
-          min={0}
-          max={durationMinutes}
-          marks={markLabels}
+          min={mainSection.start}
+          max={mainSection.end}
+          marks={mainSection.marks}
           dotStyle={{ borderColor: railColor }}
           activeDotStyle={{ borderColor: railColor }}
           railStyle={{ backgroundColor: railColor }}
           handleStyle={{ borderColor: handleColor, backgroundColor: handleColor }}
-          trackStyle={{ backgroundColor: 'transparent' }}
+          trackStyle={{ backgroundColor: railColor }}
           onAfterChange={(e) => this.handleAfterChange(e)}
           handle={(p: any) =>
             <HandleWithTooltip
               overlayFormatter={(v) => moment(minTime).add('minutes', v).format('HH:mm')}
-              overlayStyle={{zIndex: 1200}}
+              overlayStyle={{ zIndex: 1200 }}
               {...p}
             />
           }
+          style={{
+            zIndex: 105
+          }}
         />
-        
+
+        {disabledMaxSection &&
+          <DisabledSliderSection
+            fullDuration={fullDuration}
+            step={timeIncrement}
+            dots={true}
+            min={disabledMaxSection.start}
+            max={disabledMaxSection.end}
+            marks={disabledMaxSection.marks}
+          />
+        }
       </div>
     );
   }
--- src/app/components/FormElements/TimeSliderCommon.tsx
+++ src/app/components/FormElements/TimeSliderCommon.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Tooltip from 'rc-tooltip';
-import { Handle } from 'rc-slider';
+import Slider, { Handle, SliderProps } from 'rc-slider';
 import 'rc-tooltip/assets/bootstrap.css';
 import moment, { Moment } from 'moment';
 
@@ -41,31 +41,88 @@ export class HandleWithTooltip extends React.PureComponent<HandleWithTooltipProp
     }
 }
 
-export function createMarks(minTime: Moment, maxTime: Moment, timeIncrement: number) : {} {
+export interface SliderMark {
+    style: React.CSSProperties;
+    label: string;
+}
+
+export type SliderMarkCollection = { [key: number]: SliderMark };
+
+export function createMarks(minTime: Moment, maxTime: Moment, timeIncrement: number): SliderMarkCollection {
     const durationMinutes: number = moment.duration(maxTime.diff(minTime)).asMinutes();
     const numberOfMarks: number = durationMinutes / timeIncrement;
-    
+
     let markLabels = {};
     for (let i = 0; i <= numberOfMarks; i++) {
-      const index = i * timeIncrement;
-      const minuteValue = i * timeIncrement;
-      let timeLabel = moment(minTime).add('minutes', minuteValue);
-      if (timeLabel.get('minutes') === 0) {
-        markLabels[index] = {
-          style: {
-            transform: 'rotate(45deg)',
-            webkitTransform: 'rotate(45deg)',
-            msTransform: 'rotate(45deg)',
-            marginLeft: 0,
-            marginTop: -2
-          },
-          label: timeLabel.format('HH:mm')
-        };
-      } else {
-        markLabels[index] = '';
-      }
+        const index = i * timeIncrement;
+        const minuteValue = i * timeIncrement;
+        let timeLabel = moment(minTime).add('minutes', minuteValue);
+        if (timeLabel.get('minutes') === 0) {
+            markLabels[index] = {
+                style: {
+                    transform: 'rotate(45deg)',
+                    webkitTransform: 'rotate(45deg)',
+                    msTransform: 'rotate(45deg)',
+                    marginLeft: 0,
+                    marginTop: -2
+                },
+                label: timeLabel.format('HH:mm')
+            };
+        } else {
+            markLabels[index] = '';
+        }
     }
 
     return markLabels;
+}
 
+export function adjustMarks(marks: SliderMarkCollection, adjustment: number): SliderMarkCollection {
+    return Object.keys(marks)
+        .map(k => parseInt(k))
+        .reduce(
+            (newMarks, markKey) => {
+                newMarks[markKey + adjustment] = marks[markKey];
+                return newMarks;
+            },
+            {});
+}
+
+export interface DisabledSliderSectionProps extends SliderProps {
+    fullDuration: number;
+}
+
+/**
+ * A disabled slider section that can be used to represent a portion of the slider track 
+ * that cannot be selected
+ * 
+ * @export
+ * @class DisabledSliderSecion
+ * @extends {React.PureComponent<SliderProps>}
+ */
+export class DisabledSliderSection extends React.PureComponent<DisabledSliderSectionProps> {
+    render() {
+        const railColor = 'lightgray';
+        const handleColor = 'lightgray';
+        const { fullDuration, min = 0, max = 0 } = this.props;
+        const { style = {}, ...restProps } = this.props;
+        const width = `${(max - min) / fullDuration * 100}%`;
+        return (
+            <Slider
+                handle={(p) => null}
+                disabled={true}
+                dots={true}
+                dotStyle={{ borderColor: railColor }}
+                activeDotStyle={{ borderColor: railColor }}
+                railStyle={{ backgroundColor: railColor }}
+                handleStyle={{ borderColor: handleColor, backgroundColor: handleColor }}
+                trackStyle={{ backgroundColor: railColor }}
+                style={{
+                    zIndex: 100,
+                    style,
+                    width
+                }}
+                {...restProps}
+            />
+        );
+    }
 }
