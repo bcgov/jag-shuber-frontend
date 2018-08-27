2a68039b9ce04c90d13a5c7d3266e2a2c4d44170
holly.mcquay <holly.mcquay@CANL-C02VV2XJHTDD.local>
2018-08-27 07:58:22 -0700

    updating confirmation message for deleting a default assignment

--- src/app/containers/AssignmentEditModal.tsx
+++ src/app/containers/AssignmentEditModal.tsx
@@ -3,36 +3,43 @@ import {
     Button,
     Glyphicon
 } from 'react-bootstrap';
+import { RootState } from '../store';
 import AssignmentEditForm from './AssignmentEditForm';
 import ModalWrapper from './ModalWrapper/ModalWrapper';
 import { IdType } from '../api';
 import { ConfirmationModal } from '../components/ConfirmationModal';
 import { connect } from 'react-redux';
 import { deleteAssignment as deleteAssignmentById } from '../modules/assignments/actions';
+import toTitleCase from '../infrastructure/toTitleCase';
+import { getAssignment } from '../modules/assignments/selectors';
 export interface AssignmentEditModalProps {
     assignmentId: IdType;
     allowDelete?: boolean;
     allowSave?: boolean;
 }
 
+export interface AssignmentEditModalStatePros {
+    assignmentTitle?: string;
+}
 export interface AssignmentEditModalDispatchProps {
     deleteAssignment: (id: IdType) => void;
 }
 
 class AssignmentEditModal extends React.PureComponent<
-    AssignmentEditModalProps & AssignmentEditModalDispatchProps> {
+    AssignmentEditModalProps & AssignmentEditModalDispatchProps & AssignmentEditModalStatePros> {
 
     render() {
         const {
             assignmentId,
             deleteAssignment,
             allowDelete = true,
-            allowSave = true
+            allowSave = true,
+            assignmentTitle = 'Assignment'
         } = this.props;
 
         const deleteConfirmationMessage =
             // tslint:disable-next-line:max-line-length
-            <p style={{ fontSize: 14 }}>Please confirm that you would like to <b>permanently delete</b> this assignment</p>;
+            <p style={{ fontSize: 14 }}><b>This will impact your Duty Roster!</b> <br />{toTitleCase(assignmentTitle)} duties for today and all future days will be removed.</p>;
 
         return (
             <div>
@@ -71,5 +78,13 @@ class AssignmentEditModal extends React.PureComponent<
         );
     }
 }
+
+const mapStateToProps = (state: RootState, props: AssignmentEditModalProps) => {
+    const assignment = getAssignment(props.assignmentId)(state);
+    return {
+        assignmentTitle: assignment ? assignment.title : ''
+    };
+};
+
 // tslint:disable-next-line:max-line-length
-export default connect<{}, AssignmentEditModalDispatchProps, AssignmentEditModalProps>(null, { deleteAssignment: deleteAssignmentById })(AssignmentEditModal);
+export default connect<AssignmentEditModalStatePros, AssignmentEditModalDispatchProps, AssignmentEditModalProps>(mapStateToProps, { deleteAssignment: deleteAssignmentById })(AssignmentEditModal);
