553225f5560dee4a26e84dffc534907a9aefcdbb
Colter McQuay <colter.mcquay@roughdraftdev.com>
2018-09-18 09:06:29 -0700

    Updated arrayToMap - added support for two dimensional mapping

--- src/app/infrastructure/arrayToMap.ts
+++ src/app/infrastructure/arrayToMap.ts
@@ -1,7 +1,23 @@
-export default function arrayToMap<T, TKey>(array: T[], keySelector: (t: T) => TKey) {
+/**
+ * converts an Array into a Map or Dictionary type object using the given key selector to organize it
+ *
+ * @export
+ * @template T
+ * @template TKey
+ * @param {T[]} array
+ * @param {(t: T) => TKey} keySelector
+ * @param {boolean} [twoDimensional=false] creates an array for each key that is selected, pushing matching items onto array
+ * @returns
+ */
+export default function arrayToMap<T, TKey>(array: T[] = [], keySelector: (t: T) => TKey, twoDimensional: boolean = false) {
     const mappedArray = array.reduce<any>(
         (map, i) => {
-            map[keySelector(i)] = i;
+            const key = keySelector(i);
+            if (twoDimensional) {
+                (map[key] = map[key] || []).push(i);
+            } else {
+                map[key] = i;
+            }
             return map;
         },
         {});
