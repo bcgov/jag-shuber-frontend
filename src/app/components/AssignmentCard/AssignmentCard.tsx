f85ee28251d1b5c9fa7d91a770ed3fbec09541d9
Tiago <tiago.graf@gmail.com>
2019-02-15 17:19:17 -0800

    Fixes multiple duties in the same line in the duty roster causing the unassigned lines to overlap Fixes alignment of unassigned lines

--- src/app/components/AssignmentCard/AssignmentCard.tsx
+++ src/app/components/AssignmentCard/AssignmentCard.tsx
@@ -1,22 +1,26 @@
 import React from 'react';
-import { Assignment } from '../../api';
 import AssignmentDutyAddModal from '../../containers/AssignmentDutyAddModal';
 import './AssignmentCard.css';
+import { BaseAssignment, IdType } from '../../api';
+
+export interface AssignmentGroup extends BaseAssignment {
+    assignmentId: IdType;
+}
 
 export interface AssignmentCardProps {
-    assignment: Assignment;
+    assignmentGroup: AssignmentGroup;
 }
 
 export default class AssignmentCard extends React.PureComponent<AssignmentCardProps, {}> {
     render() {
-        const { assignment: { title, id } } = this.props;
+        const { assignmentGroup: { title, assignmentId } } = this.props;
         return (
             <div className="assignment-card" >
                 <div className="assignment-card-title" title={title}>
                     {title}
                 </div>
                 <div className="assignment-actions">
-                    <AssignmentDutyAddModal assignmentId={id}/>
+                    <AssignmentDutyAddModal assignmentId={assignmentId}/>
                 </div>
             </div>            
         );
