b06266e33ed7ff2d19a7d63a3e0ee9730f42d472
Lucas <tech@firebrandwebsolutions.com>
2019-12-10 15:50:54 -0800

    Add API and frontend scope grids. Need to separate form keys from form name.

---
+++ src/app/containers/AdminRolesGrid/AdminApiScopesGrid.tsx
@@ -0,0 +1,137 @@
+import React from 'react';
+import {
+    Table
+} from 'react-bootstrap';
+import {
+    FormErrors
+} from 'redux-form';
+
+import { Dispatch } from 'redux';
+
+import {
+    getApiScopes
+} from '../../modules/roles/actions';
+
+import { RootState } from '../../store';
+
+import {
+    getAllApiScopes
+} from '../../modules/roles/selectors';
+
+import { IdType } from '../../api';
+
+import {
+    FormContainerBase,
+    FormContainerProps,
+} from '../../components/Form/FormContainer';
+
+import DataTable, { EmptyDetailRow } from '../../components/Table/DataTable';
+
+import ApiScopeSelector from './ApiScopeSelector';
+import AdminRolePermissionsModal from './AdminRolePermissionsModal';
+
+export interface AdminApiScopesProps extends FormContainerProps {
+    apiScopes?: any[];
+}
+
+export interface AdminApiScopesDisplayProps extends FormContainerProps {
+
+}
+
+class AdminApiScopesDisplay extends React.PureComponent<AdminApiScopesDisplayProps, any> {
+    render() {
+        const { data = [] } = this.props;
+
+        // TODO: Rip out dummy data
+        const testData = [{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }, { id: 5 }];
+        return (
+            <div>
+                <Table responsive={true} striped={true} >
+                    <thead>
+                        <tr>
+                            <th className="text-left">Role Name</th>
+                            <th className="text-left">Role Code</th>
+                            <th className="text-left">Description</th>
+                            <th className="text-left">Date Created</th>
+                            <th className="text-left">Status</th>
+                            <th />
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {testData.map(r => {
+                            return (
+                                <tr key={r.id}>
+                                    <td>Test Role</td>
+                                    <td>TEST_ROLE</td>
+                                    <td>Ipsum Lorem Dolor</td>
+                                    <td>{new Date().toLocaleDateString()}</td>
+                                    <td>
+                                        Active
+                                    </td>
+                                </tr>
+                            );
+                        })}
+
+                    </tbody>
+                </Table>
+            </div>
+        );
+    }
+}
+
+export default class AdminApiScopesGrid extends FormContainerBase<AdminApiScopesProps> {
+    name = 'roles';
+    formFieldNames = {
+        apiScopes: 'roles.apiScopes'
+    };
+    title: string = 'Register API Routes';
+
+    FormComponent = (props: FormContainerProps<AdminApiScopesProps>) => {
+        return (
+            <div>
+                <DataTable
+                    fieldName={this.formFieldNames.apiScopes}
+                    title={''} // Leave this blank
+                    buttonLabel={'Add API Route'}
+                    displayHeaderActions={true}
+                    columns={[
+                        DataTable.SelectorFieldColumn('API Role', { fieldName: 'id', selectorComponent: ApiScopeSelector, displayInfo: true, disabled: true }),
+                        DataTable.TextFieldColumn('Code', { fieldName: 'code', displayInfo: true, disabled: true }),
+                        DataTable.TextFieldColumn('Description', { fieldName: 'description', displayInfo: true, disabled: true })
+                    ]}
+                    rowComponent={EmptyDetailRow}
+                    modalComponent={EmptyDetailRow}
+                />
+            </div>
+        );
+    }
+
+    // TODO: Figure out why Fragments aren't working...
+    DisplayComponent = (props: FormContainerProps<AdminApiScopesDisplayProps>) => (
+        <div>
+            {/*<Alert>No roles exist</Alert>*/}
+            <AdminApiScopesDisplay {...props} />
+        </div>
+    )
+
+    validate(values: AdminApiScopesProps = {}): FormErrors | undefined {
+        return undefined;
+    }
+
+    // TODO: Not sure if this should be roleId or what, I'm not there yet...
+    fetchData(roleId: IdType, dispatch: Dispatch<{}>) {
+        dispatch(getApiScopes()); // This data needs to always be available for select lists
+    }
+
+    getData(roleId: IdType, state: RootState) {
+        const apiScopes = getAllApiScopes(state) || undefined;
+        return {
+            apiScopes
+        };
+    }
+
+    getDataFromFormValues(formValues: {}): FormContainerProps {
+        return super.getDataFromFormValues(formValues) || {
+        };
+    }
+}
--- src/app/pages/ManageRoles.tsx
+++ src/app/pages/ManageRoles.tsx
@@ -1,21 +1,16 @@
 import React from 'react';
-import { Button, Glyphicon, Well } from 'react-bootstrap';
+import { Well } from 'react-bootstrap';
 
-import { IdType } from '../api';
+// import { IdType } from '../api';
 import Page from '../components/Page/Page';
-// import { FormContainerProps } from '../components/Form/FormContainer';
-
-// import SheriffList from '../containers/SheriffList';
-import AdminRolesGrid, { AdminRolesDisplayProps } from '../containers/AdminRolesGrid/AdminRolesGrid';
-// TODO: Probably should get rid of this
-// import SheriffProfileCreateModal from '../containers/SheriffProfileCreateModal';
 import AdminForm from '../containers/AdminForm';
 import { AdminFormProps } from '../components/AdminForm/AdminForm';
-import { FormContainerBase } from '../components/Form/FormContainer';
 
 // Import plugins
 import AdminRolesGridPlugin from '../containers/AdminRolesGrid/AdminRolesGrid';
-import AdminCodeTypesGridPlugin from '../containers/AdminCodeTypesGrid/AdminCodeTypesGrid';
+import AdminFrontendScopesGridPlugin from '../containers/AdminRolesGrid/AdminFrontendScopesGrid';
+import AdminApiScopesGridPlugin from '../containers/AdminRolesGrid/AdminApiScopesGrid';
+// import AdminCodeTypesGridPlugin from '../containers/AdminCodeTypesGrid/AdminCodeTypesGrid';
 
 export interface ManageRolesProps {}
 
@@ -29,20 +24,6 @@ class ManageRoles extends React.PureComponent<AdminFormProps> {
 
         this.toggleEditMode = this.toggleEditMode.bind(this);
     }
-
-    /*renderFormContainer(): {} {
-        const dataTable = new AdminRolesGrid();
-        const roleId: IdType = 'asdf-1234';
-        // const data: any = [];
-        // TODO: Replace with real data
-        const data: {}[] = [{ id: 1 }, { id: 2 }, { id: 3 }];
-
-        const tableProps: AdminRolesDisplayProps = { objectId: roleId, data: data };
-
-        // TODO: Get this working!
-        // return dataTable.renderDisplay(tableProps);
-        return dataTable.renderFormFields(tableProps);
-    }*/
 
     // TODO: What is the name of the function elsewhere?
     toggleEditMode() {
@@ -84,8 +65,9 @@ class ManageRoles extends React.PureComponent<AdminFormProps> {
                     <AdminForm
                         key={Math.random()}
                         plugins={[
-                            new AdminRolesGridPlugin(),
-                            // new AdminCodeTypesGridPlugin()
+                            new AdminApiScopesGridPlugin(),
+                            new AdminFrontendScopesGridPlugin(),
+                            new AdminRolesGridPlugin()
                         ]}
                         isEditing={isEditing}
                     />
