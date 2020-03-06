721c9820d9fbf5be7270df9a59887397cfd9f1c4
Lucas <tech@firebrandwebsolutions.com>
2020-03-05 20:19:26 -0800

    Add user profile dropdown menu and logout button. User profile display is currently faked out and not connected to redux.

--- src/app/components/Navigation.tsx
+++ src/app/components/Navigation.tsx
@@ -3,7 +3,12 @@ import {
     Nav,
     Navbar,
     NavbarBrand,
-    Badge, Glyphicon, Image
+    NavDropdown,
+    Dropdown,
+    MenuItem,
+    Badge,
+    Glyphicon,
+    Image
 } from 'react-bootstrap';
 import NavigationLink from './NavigationLink';
 import LocationSelector from '../containers/NavLocationSelector';
@@ -11,6 +16,8 @@ import bcLogo from '../assets/images/bc-logo-transparent-no-underline.png';
 import bcLogoDark from '../assets/images/bc-logo-transparent-dark.png';
 import NavigationDropDown from './NavigationDropDown';
 import avatarImg from '../assets/images/avatar.png';
+import SheriffRankDisplay from '../containers/SheriffRankDisplay';
+import SheriffDisplay from '../containers/SheriffDisplay';
 
 export interface NavigationProps {
 
@@ -135,13 +142,55 @@ export default class Navigation extends React.Component<NavigationProps, any> {
                     <Nav pullRight={true} style={{ paddingTop: 13, paddingRight: 15 }}>
                         <div className="flex-row-wrap">
                             <LocationSelector.Current />
-                            <Image
-                                style={{ marginLeft: '30px' }}
-                                src={avatarImg}
-                                circle={true}
-                                width="32"
-                                height="32"
-                            />
+                            <Dropdown id="user-profile-menu" className="">
+                                <Dropdown.Toggle className="user-profile-menu-toggle btn-transparent">
+                                     <Image
+                                        style={{ marginLeft: '30px' }}
+                                        src={avatarImg}
+                                        circle={true}
+                                        width="32"
+                                        height="32"
+                                     />
+                                </Dropdown.Toggle>
+                                <Dropdown.Menu>
+                                    <SheriffDisplay
+                                        sheriffId={''}
+                                        RenderComponent={({ sheriff: {
+                                            firstName = 'George',
+                                            lastName = 'Thurman',
+                                            imageUrl = '',
+                                            badgeNo = '123',
+                                            rankCode = 'STAFFINSPECTOR'
+                                        } = {} }) =>
+                                            (
+                                                <div className="sheriff-profile-header">
+                                                    {/* <Image
+                                                        src={imageUrl ? imageUrl : avatarImg}
+                                                        circle={true}
+                                                        width="115"
+                                                        height="115"
+                                                    /> */}
+                                                    <div style={{ marginTop: 30, fontSize: 14 }}>#{badgeNo}</div>
+                                                    <div style={{ fontWeight: 'bold', fontSize: 18 }}>
+                                                        {firstName.toUpperCase()} {lastName.toUpperCase()}
+                                                    </div>
+                                                    <div style={{ fontSize: 14 }}><SheriffRankDisplay code={rankCode} /></div>
+                                                </div>
+                                            )
+                                        }
+                                    />
+                                    <hr />
+                                    <MenuItem
+                                        style={{ textAlign: 'center' }}
+                                        onClick={() => {
+                                            // tslint:disable-next-line:max-line-length
+                                            window.location.href = `https://logon.gov.bc.ca/clp-cgi/logoff.cgi?returl=${window.location.href}`;
+                                        }}
+                                    >
+                                        Logout
+                                    </MenuItem>
+                                </Dropdown.Menu>
+                            </Dropdown>
                         </div>
                     </Nav>
                 </Navbar>
