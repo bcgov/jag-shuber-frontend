accc1f3f03d93b29dfcbb944b5ab05d9208162b3
Lucas <tech@firebrandwebsolutions.com>
2020-03-23 11:55:34 -0700

    Update LOVs for assignment types so they are sorted by the specified sort order.

--- src/app/containers/AssignmentJailRoleSelector.tsx
+++ src/app/containers/AssignmentJailRoleSelector.tsx
@@ -12,11 +12,27 @@ interface JailRoleListStateProps {
     jailRoles: JailRoleCode[];
 }
 
+// Sort by sort order, we can make this configurable later if necessary
+const sortByOrder = (a: any, b: any) => {
+    if (a.hasOwnProperty('sortOrder') && b.hasOwnProperty('sortOrder')) {
+        if (a.sortOrder < b.sortOrder) {
+            return -1;
+        } else if (b.sortOrder < a.sortOrder) {
+            return 1;
+        }
+    }
+
+    return 0;
+};
+
 class JailRoleList extends React.PureComponent<
     SelectorProps & JailRoleListStateProps> {
 
     render() {
         const { jailRoles = [], ...restProps } = this.props;
+
+        jailRoles.sort(sortByOrder);
+
         const selectorValues = jailRoles.map(role => ({ key: role.id as string, value: role.description as string }));
         return (
             <Selector {...restProps} data={selectorValues} />
