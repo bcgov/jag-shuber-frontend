ec6962a784f882472a4c8b8b171c72b17639e490
Lucas <tech@firebrandwebsolutions.com>
2020-03-30 19:20:48 -0700

    Start wiring up profile image upload.

--- src/app/modules/users/actions.ts
+++ src/app/modules/users/actions.ts
@@ -2,6 +2,7 @@ import * as userRequests from './requests';
 
 export const getUsers = userRequests.userMapRequest.actionCreator;
 export const createUser = userRequests.createUserRequest.actionCreator;
+export const uploadUserImage = userRequests.uploadUserImageRequest.actionCreator;
 export const updateUser = userRequests.updateUserRequest.actionCreator;
 export const createOrUpdateUsers = userRequests.createOrUpdateUsersRequest.actionCreator;
 export const deleteUsers = userRequests.deleteUsersRequest.actionCreator;
