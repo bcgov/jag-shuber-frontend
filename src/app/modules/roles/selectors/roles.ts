3f634c6424021cc607a29fc9f22885cb31e48bcf
Lucas <tech@firebrandwebsolutions.com>
2020-04-20 06:25:24 -0700

    Hide system roles from getAllRoles. Users cannot set Super Admin users, they are configured via OpenShift. Remove from dropdowns by adding a filter to getAllRoles.

--- src/app/modules/roles/selectors/roles.ts
+++ src/app/modules/roles/selectors/roles.ts
@@ -18,8 +18,13 @@ export const getRoles = createSelector(
 
 export const getAllRoles = (state: RootState) => {
     if (state) {
-        return getRoles(state).sort((a: any, b: any) =>
-            (a.roleName < b.roleName) ? -1 : (a.roleName > b.roleName) ? 1 : 0);
+        return getRoles(state)
+            .filter((role) => {
+                // TODO: Make this configurable so super admins can see system roles if necessary, not required for now
+                return role.systemRoleInd !== 1;
+            })
+            .sort((a: any, b: any) =>
+                (a.roleName < b.roleName) ? -1 : (a.roleName > b.roleName) ? 1 : 0);
     }
     return undefined;
 };
